a8d319a371c2a32fbbec7c6737d0f1568abc7cc2
Nathan Kinkade <nath@nkinka.de>
2018-08-15 17:59:45 -0600

    Writes site and machine state to file, and restores on startup. Uses flags package. General cleanup.

--- gmx.go
+++ gmx.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/gob"
+	"flag"
 	"log"
 	"net/http"
+	"os"
 	"regexp"
 	"strconv"
 
@@ -12,12 +15,16 @@ import (
 )
 
 var (
-	listenPort      = "9999"
-	githubSecret    = []byte("7f29588262f53e45ea1aa1da7e0f13b9105e6589")
-	maintenanceNode = prometheus.NewGaugeVec(
+	fListenAddress        string
+	fMachineStateFilePath string
+	fSiteStateFilePath    string
+
+	githubSecret = []byte("7f29588262f53e45ea1aa1da7e0f13b9105e6589")
+
+	maintenanceMachine = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
-			Name: "gmx_node_status",
-			Help: "Status of node.",
+			Name: "gmx_machine_state",
+			Help: "State of machine.",
 		},
 		[]string{
 			"machine",
@@ -26,59 +33,125 @@ var (
 	)
 	maintenanceSite = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
-			Name: "gmx_site_status",
-			Help: "Status of site.",
+			Name: "gmx_site_state",
+			Help: "State of site.",
 		},
 		[]string{
 			"site",
 			"issue",
 		},
 	)
-	nodeStatusMap = make(map[string]string)
-	siteStatusMap = make(map[string]string)
+	machineStateMap = make(map[string]string)
+	siteStateMap    = make(map[string]string)
 )
 
-func updateNodeStatus(node string, issueNumber string, action float64) {
-	// Updates the status map and Prometheus metric for the node
+func writeMachineState() {
+	machineStateFile, err := os.Create(fMachineStateFilePath)
+	if err == nil {
+		machineStateFile, err := os.Create(fMachineStateFilePath)
+		if err != nil {
+			log.Fatalf("ERROR: Failed to write file %s with error: %s", fMachineStateFilePath, err)
+		}
+		machineStateEncoder := gob.NewEncoder(machineStateFile)
+		machineStateEncoder.Encode(machineStateMap)
+		log.Println("INFO: Successfully wrote machineStateMap to disk.")
+	} else {
+		log.Fatalf("ERROR: Failed to write file %s with error: %s", fMachineStateFilePath, err)
+	}
+	machineStateFile.Close()
+}
+
+func writeSiteState() {
+	siteStateFile, err := os.Create(fSiteStateFilePath)
+	if err == nil {
+		siteStateFile, err := os.Create(fSiteStateFilePath)
+		if err != nil {
+			log.Fatalf("ERROR: Failed to write file %s with error: %s", fSiteStateFilePath, err)
+		}
+		siteStateEncoder := gob.NewEncoder(siteStateFile)
+		siteStateEncoder.Encode(siteStateMap)
+		log.Println("INFO: Successfully wrote siteStateMap to disk.")
+	} else {
+		log.Fatalf("ERROR: Failed to write file %s with error: %s", fMachineStateFilePath, err)
+	}
+	siteStateFile.Close()
+}
+
+func restoreState() {
+	machineStateFile, err := os.Open(fMachineStateFilePath)
+	if err == nil {
+		machineStateDecoder := gob.NewDecoder(machineStateFile)
+		err = machineStateDecoder.Decode(&machineStateMap)
+		if err != nil {
+			log.Fatalf("ERROR: Failed to decode %s with error: %s", fMachineStateFilePath, err)
+		}
+		for machine, issue := range machineStateMap {
+			updateMachineState(machine, issue, 1)
+		}
+		log.Println("INFO: Successfully restored machineStateMap from disk.")
+	} else {
+		log.Printf("WARNING: Failed to open %s with error: %s", fMachineStateFilePath, err)
+	}
+	machineStateFile.Close()
+
+	siteStateFile, err := os.Open(fSiteStateFilePath)
+	if err == nil {
+		siteStateDecoder := gob.NewDecoder(siteStateFile)
+		err = siteStateDecoder.Decode(&siteStateMap)
+		if err != nil {
+			log.Fatalf("ERROR: Failed to decode %s with error: %s", fSiteStateFilePath, err)
+		}
+		for site, issue := range siteStateMap {
+			updateSiteState(site, issue, 1)
+		}
+		log.Println("INFO: Successfully restored siteStateMap from disk.")
+	} else {
+		log.Printf("WARNING: Failed to open %s with error: %s", fSiteStateFilePath, err)
+	}
+	siteStateFile.Close()
+}
+
+func updateMachineState(machine string, issueNumber string, action float64) {
+	// Updates the state map and Prometheus metric for the machine
 	switch action {
 	case 0:
-		delete(nodeStatusMap, node)
-		maintenanceNode.WithLabelValues(node+".measurement-lab.org", issueNumber).Set(action)
-		log.Printf("INFO: Node %s was removed from maintenance.", node)
+		delete(machineStateMap, machine)
+		maintenanceMachine.WithLabelValues(machine+".measurement-lab.org", issueNumber).Set(action)
+		log.Printf("INFO: Machine %s was removed from maintenance.", machine)
 	case 1:
-		nodeStatusMap[node] = issueNumber
-		maintenanceNode.WithLabelValues(node+".measurement-lab.org", issueNumber).Set(action)
-		log.Printf("INFO: Node %s was added to maintenance.", node)
+		machineStateMap[machine] = issueNumber
+		maintenanceMachine.WithLabelValues(machine+".measurement-lab.org", issueNumber).Set(action)
+		log.Printf("INFO: Machine %s was added to maintenance.", machine)
 	case 2:
-		for node, issue := range nodeStatusMap {
+		for machine, issue := range machineStateMap {
 			if issue == issueNumber {
-				delete(nodeStatusMap, node)
+				delete(machineStateMap, machine)
 			}
-			maintenanceNode.WithLabelValues(node+".measurement-lab.org", issueNumber).Set(0)
-			log.Printf("INFO: Node %s was removed from maintenance because issue was closed.", node)
+			maintenanceMachine.WithLabelValues(machine+".measurement-lab.org", issueNumber).Set(0)
+			log.Printf("INFO: Machine %s was removed from maintenance because issue was closed.", machine)
 		}
 	default:
-		log.Printf("ERROR: Unknown node action type: %f", action)
+		log.Printf("ERROR: Unknown machine action type: %f", action)
 	}
 }
 
-func updateSiteStatus(site string, issueNumber string, action float64) {
-	// Updates the status map and Prometheus metric for a site
+func updateSiteState(site string, issueNumber string, action float64) {
+	// Updates the state map and Prometheus metric for a site
 	switch action {
 	case 0:
-		delete(siteStatusMap, site)
-		maintenanceNode.WithLabelValues(site, issueNumber).Set(action)
+		delete(siteStateMap, site)
+		maintenanceSite.WithLabelValues(site, issueNumber).Set(action)
 		log.Printf("INFO: Site %s was removed from maintenance.", site)
 	case 1:
-		siteStatusMap[site] = issueNumber
-		maintenanceNode.WithLabelValues(site, issueNumber).Set(action)
+		siteStateMap[site] = issueNumber
+		maintenanceSite.WithLabelValues(site, issueNumber).Set(action)
 		log.Printf("INFO: Site %s was added to maintenance.", site)
 	case 2:
-		for site, issue := range siteStatusMap {
+		for site, issue := range siteStateMap {
 			if issue == issueNumber {
-				delete(siteStatusMap, site)
+				delete(siteStateMap, site)
 			}
-			maintenanceNode.WithLabelValues(site, issueNumber).Set(0)
+			maintenanceSite.WithLabelValues(site, issueNumber).Set(0)
 			log.Printf("INFO: Site %s was removed from maintenance because issue was closed.", site)
 		}
 	default:
@@ -87,19 +160,20 @@ func updateSiteStatus(site string, issueNumber string, action float64) {
 }
 
 func parseMessage(msg string, num string) int {
-	nodeExp, _ := regexp.Compile(`\/node (mlab[1-4]{1}\.[a-z]{3}[0-9c]{2})\s?(del)?`)
+	machineExp, _ := regexp.Compile(`\/machine (mlab[1-4]{1}\.[a-z]{3}[0-9c]{2})\s?(del)?`)
 	siteExp, _ := regexp.Compile(`\/site ([a-z]{3}[0-9c]{2})\s?(del)?`)
-	nodeMatches := nodeExp.FindAllStringSubmatch(msg, -1)
-	if len(nodeMatches) > 0 {
-		for _, node := range nodeMatches {
-			log.Printf("INFO: Flag found for node: %s", node[1])
-			if node[2] == "del" {
-				log.Printf("INFO: Node %s will be removed from maintenance.", node[1])
-				updateNodeStatus(node[1], num, 0)
+	machineMatches := machineExp.FindAllStringSubmatch(msg, -1)
+	if len(machineMatches) > 0 {
+		for _, machine := range machineMatches {
+			log.Printf("INFO: Flag found for machine: %s", machine[1])
+			if machine[2] == "del" {
+				log.Printf("INFO: Machine %s will be removed from maintenance.", machine[1])
+				updateMachineState(machine[1], num, 0)
 			} else {
-				log.Printf("INFO: Node %s will be added to maintenance.", node[1])
-				updateNodeStatus(node[1], num, 1)
+				log.Printf("INFO: Machine %s will be added to maintenance.", machine[1])
+				updateMachineState(machine[1], num, 1)
 			}
+			writeMachineState()
 		}
 
 	}
@@ -110,11 +184,12 @@ func parseMessage(msg string, num string) int {
 			log.Printf("INFO: Flag found for site: %s", site[1])
 			if site[2] == "del" {
 				log.Printf("INFO: Site %s will be removed from maintenance.", site[1])
-				updateSiteStatus(site[1], num, 0)
+				updateSiteState(site[1], num, 0)
 			} else {
 				log.Printf("INFO: Site %s will be added to maintenance.", site[1])
-				updateSiteStatus(site[1], num, 1)
+				updateSiteState(site[1], num, 1)
 			}
+			writeSiteState()
 		}
 	}
 
@@ -141,8 +216,8 @@ func receiveHook(resp http.ResponseWriter, req *http.Request) {
 		issueNumber = strconv.Itoa(event.Issue.GetNumber())
 		if event.GetAction() == "closed" {
 			log.Printf("INFO: Issue #%s was closed.", issueNumber)
-			updateNodeStatus("n/a", issueNumber, 2)
-			updateSiteStatus("n/a", issueNumber, 2)
+			updateMachineState("n/a", issueNumber, 2)
+			updateSiteState("n/a", issueNumber, 2)
 			status = http.StatusOK
 		} else {
 			status = parseMessage(event.Issue.GetBody(), issueNumber)
@@ -163,12 +238,19 @@ func receiveHook(resp http.ResponseWriter, req *http.Request) {
 }
 
 func init() {
-	prometheus.MustRegister(maintenanceNode)
+	flag.StringVar(&fListenAddress, "web.listen-address", ":9999",
+		"Address to listen on for telemetry.")
+	flag.StringVar(&fMachineStateFilePath, "storage.machine-state-file", "/tmp/gmx-machine-state",
+		"Filesystem path for machine state file.")
+	flag.StringVar(&fSiteStateFilePath, "storage.site-state-file", "/tmp/gmx-site-state",
+		"Filesystem path for site state file.")
+	prometheus.MustRegister(maintenanceMachine)
 	prometheus.MustRegister(maintenanceSite)
+	restoreState()
 }
 
 func main() {
 	http.HandleFunc("/webhook", receiveHook)
 	http.Handle("/metrics", promhttp.Handler())
-	log.Fatal(http.ListenAndServe(":"+listenPort, nil))
+	log.Fatal(http.ListenAndServe(fListenAddress, nil))
 }
