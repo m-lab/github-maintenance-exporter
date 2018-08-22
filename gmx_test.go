084afe642200ffe181973f82fd2e52344fbd5d80
Nathan Kinkade <nath@nkinka.de>
2018-08-21 18:21:21 -0600

    Adds more unit tests.

--- gmx_test.go
+++ gmx_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"crypto/hmac"
 	"crypto/sha1"
 	"encoding/hex"
@@ -10,10 +11,51 @@ import (
 	"testing"
 )
 
+// Sample maintenance state as written to disk in JSON format.
+var savedState = `
+	{
+		"Machines": {
+			"mlab1.abc01.measurement-lab.org": "1",
+			"mlab2.xyz01.measurement-lab.org": "2",
+			"mlab3.def01.measurement-lab.org": "3"
+		},
+		"Sites": {
+			"abc02": "3",
+			"def02": "3",
+			"uvw03": "4",
+			"xyz03": "5"
+
+		}
+	}
+`
+
 func generateSignature(secret, msg []byte) string {
 	mac := hmac.New(sha1.New, secret)
 	mac.Write(msg)
 	return "sha1=" + hex.EncodeToString(mac.Sum(nil))
+}
+
+func TestRestoreState(t *testing.T) {
+	expectedMachines := 3
+	expectedSites := 4
+
+	r := bufio.NewReader(strings.NewReader(savedState))
+	testState := maintenanceState{
+		Machines: make(map[string]string),
+		Sites:    make(map[string]string),
+	}
+
+	restoreState(r, &testState)
+
+	if len(testState.Machines) != expectedMachines {
+		t.Errorf("restoreState(): Expected %d restored machines; have %d.",
+			expectedMachines, len(testState.Machines))
+	}
+
+	if len(testState.Sites) != expectedSites {
+		t.Errorf("restoreState(): Expected %d restored sites; have %d.",
+			expectedSites, len(testState.Sites))
+	}
 }
 
 func TestReceiveHook(t *testing.T) {
@@ -48,16 +90,24 @@ func TestReceiveHook(t *testing.T) {
 			secretKey:      []byte("badsecret"),
 			eventType:      "issues",
 			expectedStatus: http.StatusUnauthorized,
-			payload:        []byte(`{"key":"value"}`),
+			payload:        []byte(`{"fake":"data"}`),
+		},
+		{
+			name:           "issues-hook-unsupported-event-type",
+			secretKey:      githubSecret,
+			eventType:      "pull_request",
+			expectedStatus: http.StatusNotImplemented,
+			payload:        []byte(`{"fake":"data"}`),
 		},
 		{
 			name:           "issue-comment-hook-malformed-payload",
 			secretKey:      githubSecret,
 			eventType:      "issue_comment",
 			expectedStatus: http.StatusBadRequest,
-			payload:        []byte(`{ "malformed; 'json }]]}`),
+			payload:        []byte(`"malformed; 'json }]]}`),
 		},
 	}
+
 	for _, test := range tests {
 		sig := generateSignature(test.secretKey, test.payload)
 		req, err := http.NewRequest("POST", "/webhook", strings.NewReader(string(test.payload)))
@@ -75,6 +125,65 @@ func TestReceiveHook(t *testing.T) {
 		if status := rec.Code; status != test.expectedStatus {
 			t.Errorf("receiveHook(): test %s: wrong HTTP status: got %v; want %v",
 				test.name, rec.Code, test.expectedStatus)
+		}
+	}
+}
+
+func TestCloseIssue(t *testing.T) {
+	expectedMods := 3
+
+	r := bufio.NewReader(strings.NewReader(savedState))
+	testState := maintenanceState{
+		Machines: make(map[string]string),
+		Sites:    make(map[string]string),
+	}
+	restoreState(r, &testState)
+
+	mods := closeIssue("3", &testState)
+
+	if mods != expectedMods {
+		t.Errorf("closeIssue(): Expected %d state modifications; got %d", expectedMods, mods)
+	}
+}
+
+func TestParseMessage(t *testing.T) {
+	r := bufio.NewReader(strings.NewReader(savedState))
+	testState := maintenanceState{
+		Machines: make(map[string]string),
+		Sites:    make(map[string]string),
+	}
+	restoreState(r, &testState)
+
+	tests := []struct {
+		msg          string
+		expectedMods int
+	}{
+		{
+			msg:          `/machine mlab1.abc01 is in maintenance mode.`,
+			expectedMods: 1,
+		},
+		{
+			msg:          `Putting /site abc01 and /site xyz02 into maintenance mode.`,
+			expectedMods: 2,
+		},
+		{
+			msg:          `Putting /site abc01 and /machine mlab1.xyz02 into maintenance mode.`,
+			expectedMods: 2,
+		},
+		{
+			msg:          `Removing /machine mlab2.xyz01 del and /site uvw02 del from maintenance.`,
+			expectedMods: 2,
+		},
+		{
+			msg:          `Add /machine and /site vw02 to maintenance. Removing /site lol del.`,
+			expectedMods: 0,
+		},
+	}
+
+	for _, test := range tests {
+		mods := parseMessage(test.msg, "99", &testState)
+		if mods != test.expectedMods {
+			t.Errorf("parseMessage(): Expected %d state modifications; got %d", test.expectedMods, mods)
 		}
 	}
 }
