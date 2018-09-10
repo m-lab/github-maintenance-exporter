ad080ad50216938a6a7b79e3005c1a7648b8a5e8
Nathan Kinkade <nath@nkinka.de>
2018-09-10 16:26:18 -0600

    Changes the temporary GMX webhook secret file name to match the one that kubectl is looking for.

--- deploy_gmx.sh
+++ deploy_gmx.sh
@@ -5,7 +5,7 @@ set -e
 set -u
 
 # Write secret to a file to prevent printing secret in travis logs.
-( set +x; echo "${GITHUB_WEBHOOK_SECRET}" > /tmp/gmx-webhook-secret.txt )
+( set +x; echo "${GITHUB_WEBHOOK_SECRET}" > /tmp/gmx-webhook-secret )
 
 # Create a k8s secret for the GitHub webhook shared secret.
 kubectl create secret generic gmx-webhook-secret \
