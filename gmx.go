2ffd9351277176c6632afab1424ab5a6d50c132a
Peter Boothe <pboothe@google.com>
2019-11-06 17:55:51 -0500

    Added comments, fixed misspellings, changed an assign to _ (#32)

--- gmx.go
+++ gmx.go
@@ -1,21 +1,6 @@
-// Copyright 2018 github-maintenance-exporter Authors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//////////////////////////////////////////////////////////////////////////////
-
 // A simple Prometheus exporter that receives Github webhooks for issues and
 // issue_comments events and parses the issue or comment body for special flags
-// that indicate that a machine or site should be in maintenace mode.
+// that indicate that a machine or site should be in maintenance mode.
 package main
 
 import (
